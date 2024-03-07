fb4dabf11b0eef0d2163c0cb40a83a952edf6005
Stanko <stanko.trcek@gmail.com>
2024-03-07 22:06:37 +0100

    Dodana baza in prikaz price liste.

--- pages/4_Dashboard.py
+++ pages/4_Dashboard.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import duckdb 
+import pandas as pd
+
 
 st.write("test")
+
+con = duckdb.connection('data/portfelj.ddb')
+
+df = con.sql("SELECT * FROM price;")
+
+st.write(df)
+
+con.close()
